d3d91d7fed14aad98d4795293f9a513dee6ec635
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-15 15:19:04 +0000

    Deploy uofa-cmput404/cmput404-slides to github.com/uofa-cmput404/cmput404-slides.git:gh-pages

--- fiddler.js
+++ fiddler.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var assert = chai.assert;
+const tabWidth = 2;
 
 function extract_text(e) {
   let text = "";
@@ -59,5 +60,46 @@ function fiddler() {
         newButton.style.marginBottom = (newButton.clientHeight * -1) + 'px';
       }, 0);
     }
+  }
+}
+
+function stripPreIndentation( options ) {
+  let codes = document.querySelectorAll('pre>code');
+  for (let code of codes) {
+    let pre = code.parentElement;
+    assert(pre.nodeName === 'PRE');
+    let guts = code.innerHTML;
+    let lines = guts.split(/[\r\n]+/); // https://stackoverflow.com/a/5035058 2019-02-13
+//     console.log(lines);
+    if (lines[0].match(/^[\s]*$/)) {
+      lines.shift();
+    }
+    if (lines[lines.length-1].match(/^[\s]*$/)) {
+      lines.pop();
+    }
+    let minIndent = Infinity;
+    for (let line of lines) {
+      line = line.replace(/\t/g, ' '.repeat(tabWidth));
+//       console.log(line);
+      let indented = (line.match(/^ *[^ ]+/));
+      if (indented) {
+        indented = indented[0];
+        let indent = (indented.match(/^ */))[0].length;
+        if (indent < minIndent) {
+          minIndent = indent;
+        }
+      }
+    }
+    if (minIndent === Infinity) {
+      minIndent = 0;
+    }
+    let indentRegex = RegExp('^' + ' '.repeat(minIndent));
+    let newLines = [];
+    for (let line of lines) {
+      let newLine = line.replace(indentRegex, '');
+      newLines.push(newLine);
+    }
+    let newGuts = newLines.join("\n");
+    code.innerHTML = newGuts;
   }
 }
