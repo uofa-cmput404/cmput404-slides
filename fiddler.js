eaa76562e5a6f805a97cc7d0daa713106bebb74f
Hazel Victoria Campbell <hazel.campbell@ualberta.ca>
2019-02-15 13:04:32 -0700

    scripts

--- fiddler.js
+++ fiddler.js
@@ -146,7 +146,7 @@ window.addEventListener("load", (event) => {
   var link = document.createElement( 'link' );
   link.rel = 'stylesheet';
   link.type = 'text/css';
-  link.href = window.location.search.match( /print-pdf/gi ) ? 'node_modules/reveal.js/css/print/paper.css' : 'node_modules/reveal.js/css/print/paper.css';
+  link.href = window.location.search.match( /print-pdf/gi ) ? 'node_modules/reveal.js/css/print/pdf.css' : 'node_modules/reveal.js/css/print/paper.css';
   document.getElementsByTagName( 'head' )[0].appendChild( link );
   /* end from reveal.js */
 
